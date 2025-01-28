dddedc5d110a9d865f0ec730c7ef52d50fccce8f
kanyora-n <kanyorawangu8424@gmail.com>
2025-01-28 06:16:58 -0500

    Implemented and tested API endpoints for secondChance service

--- secondChance-backend/routes/authRoutes.js
+++ secondChance-backend/routes/authRoutes.js
@@ -54,4 +54,51 @@ router.post('/register', async (req, res) => {
     }
 });
 
+// Login Endpoint
+router.post('/login', async (req, res) => {
+    try {
+        // Task 1: Connect to `secondChance` in MongoDB through `connectToDatabase` in `db.js`.
+        const db = await connectToDatabase();
+
+        // Task 2: Access MongoDB `users` collection
+        const collection = db.collection('users');
+
+        // Task 3: Check for user credentials in database
+        const theUser = await collection.findOne({ email: req.body.email });
+        if (!theUser) {
+            logger.error('User not found');
+            return res.status(404).json({ error: 'User not found' });
+        }
+
+        // Task 4: Check if the password matches the encrypted password
+        const passwordMatch = await bcryptjs.compare(req.body.password, theUser.password);
+        if (!passwordMatch) {
+            logger.error('Passwords do not match');
+            return res.status(401).json({ error: 'Wrong password' });
+        }
+
+        // Task 5: Fetch user details
+        const userName = theUser.firstName;
+        const userEmail = theUser.email;
+
+        // Task 6: Create JWT authentication with `user._id` as payload
+        const payload = {
+            user: {
+                id: theUser._id.toString(),
+            },
+        };
+        const authtoken = jwt.sign(payload, JWT_SECRET, { expiresIn: '1h' });
+
+        // Respond with user details and token
+        logger.info('User logged in successfully');
+        res.json({ authtoken, userName, userEmail });
+    } catch (e) {
+        logger.error(`Internal server error: ${e.message}`);
+        return res.status(500).send('Internal server error');
+    }
+});
+
+
+
+
 module.exports = router;
