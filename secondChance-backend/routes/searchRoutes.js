f24a8d3330967de0d339d44ec0cd2238ed51fce6
kanyora-n <kanyorawangu8424@gmail.com>
2025-04-15 10:38:00 +0300

    Update searchRoutes.js

--- secondChance-backend/routes/searchRoutes.js
+++ secondChance-backend/routes/searchRoutes.js
@@ -1,39 +1,39 @@
-const express = require('express');
-const router = express.Router();
-const connectToDatabase = require('../models/db');
+const express = require('express')
+const router = express.Router()
+const connectToDatabase = require('../models/db')
 
 // Search for gifts
 router.get('/', async (req, res, next) => {
-    try {
-      const db = await connectToDatabase()
-      const collection = db.collection('gifts')
-  
-      // Initialize the query object
-      const query = {}
-  
-      if (req.query.name) {
-        query.name = { $regex: req.query.name, $options: 'i' } // Partial match, case-insensitive
-      }
-  
-      if (req.query.category) {
-        query.category = req.query.category
-      }
-  
-      if (req.query.condition) {
-        query.condition = req.query.condition
-      }
-  
-      if (req.query.age_years) {
-        query.age_years = { $lte: parseInt(req.query.age_years, 10) }
-      }
-  
-      // Fetch filtered gifts
-      const gifts = await collection.find(query).toArray()
-  
-      res.json(gifts)
-    } catch (e) {
-      next(e)
-    }
-  })
+  try {
+    const db = await connectToDatabase()
+    const collection = db.collection('gifts')
 
-module.exports = router;
+     // Initialize the query object
+     const query = {}
+
+     if (req.query.name) {
+    query.name = { $regex: req.query.name, $options: 'i' } // Partial match, case-insensitive
+    }
+
+  if (req.query.category) {
+       query.category = req.query.category
+   }
+  
+   if (req.query.condition) {
+    query.condition = req.query.condition
+    }
+
+     if (req.query.age_years) {
+       query.age_years = { $lte: parseInt(req.query.age_years, 10) }
+  }
+
+  // Fetch filtered gifts
+     const gifts = await collection.find(query).toArray()
+
+  res.json(gifts)
+} catch (e) {
+  next(e)
+  }
+ })
+
+module.exports = router
